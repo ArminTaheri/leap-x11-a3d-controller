18786532b64bd46c52b422deb8f7ac520331423b
ArminTaheri <armin.ytaheri@gmail.com>
2017-08-28 15:55:09 -0400

    Seperatate controller actions into self contained object

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const { exec } = require('child_process');
 const Leap = require('leapjs');
 const { map } = require('ramda');
 
+const ON_DEATH = require('death')({ SIGHUP: true });
+
 const XEventManager = require('./lib/XEventManager');
-const { ScreenState } = require('./lib/ScreenState');
+const ViewportController = require('./lib/ViewportController');
+
 const { leapControllerInit, leapAction } = require('./lib/leap-controller');
 const { log, time } = require('./lib/logger');
 
 function controllerLoop(eventManager) {
-  const screenState = new ScreenState();
-  leapControllerInit(eventManager, screenState);
+  const viewportController = new ViewportController('TOP_LEFT', eventManager);
   return () => {
     let lastFrame;
     Leap.loop({
@@ -19,14 +21,14 @@ function controllerLoop(eventManager) {
           lastFrame = frame;
           return;
         }
-        leapAction(eventManager, screenState, lastFrame, frame);
-       // log(`Time per frame: ${time()} ms`);
+        leapAction(viewportController, lastFrame, frame);
+        // log(`Time per frame: ${time()} ms`);
         lastFrame = frame;
       },
     });
   };
 }
-
+process.on('unhandledRejection', r => console.log(r));
 x11.createClient((err, display) => {
   const X = display.client;
   exec("xwininfo | grep 'xwininfo: Window id:' | awk '{print $4}'", (err, stdout) => {
@@ -36,6 +38,12 @@ x11.createClient((err, display) => {
     const wid = Number(stdout.trim());
     const em = new XEventManager();
     em.init(wid, X).then(() => {
+      ON_DEATH(() => {
+        em.mouseUp(1);
+        em.mouseUp(2);
+        em.mouseUp(3);
+        process.exit();
+      });
       X.SetInputFocus(wid);
       setTimeout(controllerLoop(em), 300);
     });
